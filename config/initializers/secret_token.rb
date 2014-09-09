efb13e89ee0f86e5fa3f3d44377f21e57fbfe5cc
dwelsh2 <dwelsh2@umbc.edu>
2014-09-08 20:47:37 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwelsh2DemoApp::Application.config.secret_key_base = '5ade8620bdb4ad73e6606c77b82ca860888e2d9c72647010e96b619ac6562e825f5eeb5bcd4dcaa74728fcb9d18df33dbddf244a9d73633f3aba5607d175020c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dwelsh2DemoApp::Application.config.session_store :cookie_store, key: '_dwelsh2_demo_app_session'
